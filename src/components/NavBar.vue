185129bb8d877c2fa2cf127b59021ae68391e591
Siegrain4 <reykazuko@gmail.com>
2024-04-28 21:59:06 +0700

    a

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -6,27 +6,6 @@
         <v-btn text class="ml-2" to="/">Movies</v-btn>
         <v-btn text class="ml-2" to="/show">TV SHOW</v-btn>
         <v-spacer></v-spacer>
-        <v-container >
-            <v-layout row wrap id="search-content">
-              <v-flex xs12 sm6 offset-sm3>
-                <v-text-field
-                
-                  v-if="showSearch"
-                  v-model="searchQuery"
-                  append-icon="mdi-magnify"
-                  @click:append="toggleSearch"
-                  label="Search"
-                  solo-inverted
-                ></v-text-field>
-                <v-icon
-                  v-else
-                  @click="toggleSearch"
-                >
-                  mdi-magnify
-                </v-icon>
-              </v-flex>
-            </v-layout>
-          </v-container>
     </v-app-bar>
     
     
